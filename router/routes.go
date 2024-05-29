edf8fb94a9e8bc0743e0db26bf2db0e03cba90d9
Thiago <devt.calazans@gmail.com>
2024-05-29 18:55:44 -0300

    feat: documentation settings

--- router/routes.go
+++ router/routes.go
@@ -2,12 +2,17 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	docs "github.com/thiagoCalazans-dev/go-api-opportunities.git/docs"
 	"github.com/thiagoCalazans-dev/go-api-opportunities.git/handler"
 )
 
 func initializeRoutes(r *gin.Engine) {
 	handler.Init()
-	v1 := r.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := r.Group(basePath)
 	{
 		v1.GET("/opportunity", handler.ShowOportunity)
 		v1.POST("/opportunity", handler.CreateOportunity)
@@ -15,4 +20,6 @@ func initializeRoutes(r *gin.Engine) {
 		v1.DELETE("/opportunity", handler.RemoveOportunity)
 		v1.GET("/opportunities", handler.ListOportunity)
 	}
+	//initialize swagger
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
